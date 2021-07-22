dc230b2230500d3b2598a7864c332b329110717a
Syuparn <s.hello.spagetti@gmail.com>
2021-07-22 20:55:55 +0900

    init project

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Card from "./Card.svelte";
+	export let words: {frontWord: string, backWord: string}[];
+</script>
+
+<main>
+	<Card frontWord={words[0].frontWord} backWord={words[0].backWord}></Card>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let frontWord: string;
+	export let backWord: string;
+    let turnedOver = false;
+    
+    function flip() {
+        turnedOver = !turnedOver;
+    }
+</script>
+
+<button class="{turnedOver ? 'back-card' : 'front-card'}" on:click="{flip}">
+    {turnedOver ? backWord : frontWord}
+</button>
+
+<style>
+    button {
+		text-transform: uppercase;
+		font-size: 6em;
+		font-weight: 100;
+        border-width: 0.03em;
+        border-radius: 0.3em;
+    }
+
+    .front-card {
+        color: #ff3e00;
+        background-color: white;
+        border-color: #ff3e00;
+    }
+
+    .back-card {
+        color: white;
+        background-color: #ff3e00;
+        border-color: #ff3e00;
+    }
+</style>
+
